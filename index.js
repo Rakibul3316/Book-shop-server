025d106d4fae14feb67c7f1a0101c831059d38b2
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2021-04-02 23:09:47 +0600

    solve the .env file probelm

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require("mongodb").ObjectID
-require('dotenv').config
+require('dotenv').config();
 const port = process.env.PORT || 5000
-// console.log(process.env.DB_USER)
+
+console.log(process.env.DB_USER)
 
 
 const app = express()
@@ -14,7 +15,8 @@ app.use(bodyParser.json())
 
 
 
-const uri = "mongodb+srv://bookShop-assign-10:7KYrGtFYB0WCSr55@cluster0.dfncg.mongodb.net/bookShop?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dfncg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     console.log("checking error", err)
